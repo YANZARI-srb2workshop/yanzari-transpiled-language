56b9a71dcbd4e057a0ac86983b98739c3c5500b2
Yanzari <ayrnconversas@gmail.com>
2026-09-06 13:57:41 -0300

    I added the Scanner Enums.
    I added important enums for the scanner.
    for example: comment enums, token type enums, etc...

---
+++ src/Scanner/enum.lua
@@ -0,0 +1,100 @@
+--> By Yanzari
+
+-- Enum Module
+local enum = require("src.libs.enum")
+
+-- Kinds of Tokens
+local TokenKinds = enum.new(
+    'String', -- "1"
+    'Template', -- `1`
+    'Integer', -- 1
+    'Float', -- 1.2
+    'Exponent', -- 1e2
+    'Hexadecimal', -- 0xAA 0xBB
+    'Binary', -- 0b00110011
+    'Identifier', -- Flower
+    'BitString', -- b"hihi"
+    'Keyword', -- is
+    'Operators', -- +
+    'Indent',
+    'Dedent',
+    'NewLine',
+    'Comment', -- //
+    'Directive', -- //!
+    'Property', -- //$
+    'Documentation', -- //@
+    'EndOfLine' -- EOF
+)
+
+-- Commentary Sizes
+local CommentSize = enum.new(
+    'Short', -- //
+    'Long' -- /* */
+)
+
+-- Miscellanious Token Kinds
+local MiscellaniousTokenKinds = enum.new(
+    'Parentesis',
+    'CurlyBrackets',
+    'SquareBrackets',
+    'Semicolon'
+)
+
+-- Operators Token Kinds
+local OperatorsTokenKinds = enum.new(
+    'Plus', -- +
+    'Plus Plus', -- ++
+    'Minus', -- -
+    'Minus Minus', -- --
+    'Asterisk', -- *
+    'Slash', -- /
+    'Percent', -- %
+    'Asterisk Asterisk', -- **
+    'Colon', -- :
+    'Colon Colon', -- ::
+    'Comma', -- ,
+    'Dot', -- .
+    'VerticalLine', -- |
+    'Ampersand', -- &
+    'Circumflex', -- ^
+    'VerticalLine VerticalLine', -- ||
+    'Ampersand Ampersand', -- &&
+    'Circumflex Circumflex', -- ^^
+    'LessThan', -- <
+    'GreaterThan', -- >
+    'LessThan LessThan', -- <<
+    'GreaterThan GreaterThan', -- >>
+    'LessThan Equals', -- <=
+    'GreaterThan Equals', -- >=
+    'LessThan Equals GreaterThan', -- <=
+    'Equals Equals', -- ==
+    'Equals', -- =
+    'Plus Equals', -- +=
+    'Minus Equals', -- -=
+    'Asterisk Equals', -- *=
+    'Slash Equals', -- /=
+    'Percent Equals', -- %=
+    'Asterisk Asterisk Equals', -- **=
+    'VerticalLine Equals', -- |=
+    'Ampersand Equals', -- &=
+    'Circumflex Equals', -- ^=
+    'Exclamation', -- !
+    'Exclamation Equals', -- !=
+    'Tilde' -- ~
+)
+
+-- Directions of Tokens (e.g. Left Parentesis, Left Curly Brackets)
+local TokenDirection = enum.new(
+    'Left',
+    'Right'
+)
+
+-- Module
+local Enums = {
+    TokenKinds=TokenKinds, -- Kinds of Tokens
+    MiscellaniousTokenKinds=MiscellaniousTokenKinds, -- Miscellanious Token Kinds
+    OperatorsTokenKinds=OperatorsTokenKinds,
+    CommentSize=CommentSize,
+    TokenDirection=TokenDirection -- Directions of Tokens (e.g. Left Parentesis, Left Curly Brackets)
+}
+return Enums
---
+++ src/libs/enum.lua
@@ -0,0 +1,40 @@
+--> By Yanzari
+
+-- Enums
+---@class Enum
+---@field key table
+---@field value any[]
+---@field new fun(...): Enum
+---@field getvalue fun(self:Enum,key:any):number
+---@field getkey fun(self:Enum,value:number):any
+local Enum = {}
+Enum.__index = Enum
+
+-- Create a Enum
+---@vararg any
+function Enum.new(...)
+    local args,output = {...},{...}
+    for k,v in ipairs(args) do
+        output[v] = k
+    end
+    local self = setmetatable({},Enum)
+    self.key = output
+    self.value = args
+    return self
+end
+
+-- Get a Value from a Key
+---@param key any the Key for getting the Value
+---@return number value the Value returned
+function Enum:getvalue(key)
+    return self.value[key]
+end
+
+-- Get a Key from a Value
+---@param value number the Value for getting a Key
+---@return any key the Key returned
+function Enum:getkey(value)
+    return self.key[value]
+end
+
+return Enum
